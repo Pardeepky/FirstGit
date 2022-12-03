02e4d6ab58b7f7bdbf60b765b5fc1c0a56749c35
pardeepky <pardeep.pky@gmail.com>
2022-12-03 21:46:34 +0530

    added 2 files for dom task

---
+++ SecondGit/dom.js
@@ -0,0 +1,13 @@
+//getElementById
+
+let headerTitle = document.getElementById('header-title')
+headerTitle.style.borderBottom = 'solid 3px #000';
+// var header = document.getElementById('main-header')
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerText = 'GoodBye';
+// headerTitle.innerHTML = '<h3>Hello</h3>'
+
+//getElementByClassname
+var title = document.getElementsByClassName('title')
+title[0].style.fontWeight = 'bold';
+title[0].style.color = 'green';
