75340e76cce2dab6d151c667c7314c1682f0436d
pardeepky <pardeep.pky@gmail.com>
2022-12-03 21:55:17 +0530

    get element by classname

--- SecondGit/dom.js
+++ SecondGit/dom.js
@@ -11,3 +11,12 @@ headerTitle.style.borderBottom = 'solid 3px #000';
 var title = document.getElementsByClassName('title')
 title[0].style.fontWeight = 'bold';
 title[0].style.color = 'green';
+
+var items = document.getElementsByClassName('list-group-item');
+items[1].textContent = 'Hello 2';
+items[1].style.fontWeight = 'bold';
+items[2].style.backgroundColor = 'green';
+
+for(var i = 0; i<items.length; i++){
+    items[i].style.fontWeight = 'bold';
+}
