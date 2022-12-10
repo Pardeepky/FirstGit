e310c6870008ac6b308ab260b2cdeb2a45c27cff
pardeepky <pardeep.pky@gmail.com>
2022-12-10 23:09:05 +0530

    query selector and queryselectorall

--- SecondGit/dom.js
+++ SecondGit/dom.js
@@ -23,12 +23,51 @@
 
 //getElementByTagName
 
-var li = document.getElementsByTagName('li');
-li[1].textContent = 'Hello 2';
-li[1].style.fontWeight = 'bold';
-li[2].style.backgroundColor = 'green';
-li[4].style.backgroundColor = 'red';
+// var li = document.getElementsByTagName('li');
+// li[1].textContent = 'Hello 2';
+// li[1].style.fontWeight = 'bold';
+// li[2].style.backgroundColor = 'green';
+// li[4].style.backgroundColor = 'red';
 
-for(var i = 0; i<li.length; i++){
-    li[i].style.fontWeight = 'bold';
+// for(var i = 0; i<li.length; i++){
+//     li[i].style.fontWeight = 'bold';
+// }
+
+// query selector
+
+var header = document.querySelector("#main-header");
+header.style.borderBottom = 'solid 4px #ccc';
+
+var input = document.querySelector('input');
+input.value = 'Hello World';
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value="Send";
+
+var item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color = 'blue';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color = 'coral';
+secondItem.style.backgroundColor = 'green';
+
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.visibility = 'hidden' 
+
+//querySelectorAll
+
+var titles = document.querySelectorAll('.title');
+titles[0].textContent = 'Hello';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
+
+for(var i = 0; i< odd.length; i++){
+    odd[i].style.backgroundColor = 'green';
 }
+
+var items = document.querySelectorAll('.list-group-item');
+items[1].style.color = 'green';
